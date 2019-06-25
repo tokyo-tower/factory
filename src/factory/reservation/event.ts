b40defef9893e9fae5563df245403563f751124e
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-06-25 06:23:26 +0000

    ### Removed - 予約インターフェースから不要な属性を削除

--- src/factory/reservation/event.ts
+++ src/factory/reservation/event.ts
@@ -1,10 +1,6 @@
-import PaymentMethodType from '../paymentMethodType';
-import * as PerformanceFactory from '../performance';
 import { IPropertyValue } from '../propertyValue';
 import * as ReservationFactory from '../reservation';
 
-import { IBilingualString } from '../multilingualString';
-import { ITicketTypeExtension } from '../offer/seatReservation';
 import { Group as PersonGroup } from '../person';
 import { IAgent } from '../transaction/placeOrder';
 
@@ -24,41 +20,8 @@ export interface ICheckin {
  * イベント予約インターフェース
  */
 export interface IReservation extends ReservationFactory.IReservation {
-    transaction: string;
-    order_number: string;
-    qr_str: string;
-    performance: string;
-    seat_code: string;
-    performance_ttts_extension: PerformanceFactory.IExtension;
-    purchaser_group: PersonGroup;
-    purchaser_name: string;
-    purchaser_last_name: string;
-    purchaser_first_name: string;
-    purchaser_email: string;
-    purchaser_tel: string;
-    purchaser_international_tel: string;
-    purchaser_age: string;
-    purchaser_address: string;
-    purchaser_gender: string;
-    payment_no: string;
-    payment_seat_index: number;
-    purchased_at: Date;
-    payment_method: PaymentMethodType;
-    seat_grade_name: IBilingualString;
-    seat_grade_additional_charge: number;
-    ticket_ttts_extension: ITicketTypeExtension;
-    rate_limit_unit_in_seconds: number;
-    watcher_name: string;
-    watcher_name_updated_at: Date;
-    charge: number;
-    owner_username?: string;
-    owner_name?: string;
-    owner_last_name?: string;
-    owner_first_name?: string;
-    owner_email?: string;
-    owner_group?: string;
     checkins: ICheckin[];
-    gmo_order_id: string;
+    purchaser_group: PersonGroup;
     transaction_agent: IAgent;
 }
 
