938ba03a1c79a2773e4de4711caf21acdccced42
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2021-01-27 00:45:59 +0000

    - パフォーマンスインターフェースを削除

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@
  */
 import * as cinerino from '@cinerino/factory';
 
-import * as PerformanceFactory from './factory/performance';
 import * as OrderReportFactory from './factory/report/order';
 
 import ErrorCode from './factory/errorCode';
@@ -12,7 +11,6 @@ import * as errors from './factory/errors';
 export import chevre = cinerino.chevre;
 export import errors = errors;
 export import errorCode = ErrorCode;
-export import performance = PerformanceFactory;
 export namespace report {
     export import order = OrderReportFactory;
 }
